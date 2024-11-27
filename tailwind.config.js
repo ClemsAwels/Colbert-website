ab8744d0405c3f11dbc37b429fcbb449c8c4791d
Clement-Besnard <clems.2048@gmail.com>
2024-11-27 17:00:54 +0100

    add : react server & app colbert chatbot page

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ajoutez vos fichiers source React
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
